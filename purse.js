8ee24e694a1223547c7632265c60d3dadeb4dfb4
Joe ST <joe@fbstj.net>
2019-07-14 16:52:21 +0100

    [purse] add import/export button creators
    * import will override purse.data with the selected file
    * export will export to 'data.json' on click

--- purse.js
+++ purse.js
@@ -25,4 +25,28 @@ class Purse {
 		}
 		this.load()
 	}
+	
+	/// create an import button to load data into this purse
+	make_import_button() {
+		const purse = this
+		const self = make_import_element()
+		self.picker.setAttribute('accept', '.json')
+		self.picker.addEventListener('change', async function (ev) {
+			console.warn('importing', this.files, 'into Purse', purse)
+			purse.data = await this.files[0].json()
+			purse.save(true)
+		})
+		return self
+	}
+	
+	/// create an export button for downloading the purse to be imported later
+	make_export_button() {
+		const purse = this
+		const self = make_export_element(true)
+		self.addEventListener('export', function (ev) {
+			console.warn('exporting', purse.data) 
+			self.attach_file('data.json', purse.data)
+		})
+		return self
+	}
 }
