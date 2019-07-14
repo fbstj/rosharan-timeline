c8051497f16482541c5516dad615b15bb5116028
Joe ST <joe@fbstj.net>
2019-07-14 14:54:44 +0100

    [export] added async handling for export button

--- user-file-buttons.js
+++ user-file-buttons.js
@@ -1,3 +1,6 @@
+///# import and export buttons
+console.debug('# user-file-buttons.js')
+
 /// creates the file-dialog opening button
 function make_import_element() {
 	// build file picker element
@@ -33,29 +36,48 @@ function read_text_file(file) { return new Promise((resolve, reject) => {
 File.prototype.text = function() { return read_text_file(this) }
 File.prototype.json = function() { return this.text().then(txt => JSON.parse(txt)) }
 
-/// creates the button which opens the file exporter
-function make_export_element() {
-	let gen_data = () => console.error('export:', 'no file loader');
+/// creates the button which saves generated files
+/// * when @is_async is true, clicking the button generates an 'export' event
+/// * the 'export' event should call `attach_file` with the generated data
+/// * when @is_async is false, the button should be disabled before `attach_file` is called
+function make_export_element(is_async = false) {
 	// build save file clicker
 	const link = document.createElement('a')
 	link.download = 'export.json'
 	// build picker open button
 	const button = document.createElement('button')
 	button.innerText = 'export'
-	button.addEventListener('click', _ => { link.click() })
-	button.disabled = true
 	// build wrapper element
 	const self = document.createElement('label')
 	self.classList.add('exporter')
 	self.append(button, link)
 	/// set up the url for downloading 
-	self.setup = function(name, data) {
-		const file = new Blob([ JSON.stringify(data) ])
+	self.attach_file = function(name, data) {
+		const file = new Blob([ JSON.stringify(data) ], 'application/json')
 		window.URL.revokeObjectURL(link.href)
 		link.download = name
 		link.href = window.URL.createObjectURL(file)
 		button.disabled = false
 	}
+	// chose between sync & async modes
+	if (is_async) {
+		self.setAttribute('async','')
+	} else {
+		button.disabled = true
+	}
+	// setup button click handler
+	button.addEventHandler('click', function(ev) {
+		// configured to be async so execute the export handler
+		if (self.hasAttribute('async')) {
+			// NOTE: this handler should call self.attach_file()
+			self.dispatchEvent('export')
+			// TODO: check this happens sync & before generated click
+		}
+		if (!link.href) {
+			console.warn('nothing to download')
+		}
+		link.click()
+	})
 	// helper accessors
 	return Object.freeze(self)
 }
